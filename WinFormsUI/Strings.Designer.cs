05a52157f6b82d13e3817c47298a0f1c771d7898
tvidvei <tor.vidvei@gmail.com>
2025-05-10 04:00:52 +0200

    Simplified structure in WinFormsUI: Removed the Docking folder

--- WinFormsUI/Strings.Designer.cs
+++ WinFormsUI/Strings.Designer.cs
@@ -39,7 +39,7 @@ namespace WeifenLuo.WinFormsUI.Docking {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WeifenLuo.WinFormsUI.Docking.Strings", typeof(Strings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WeifenLuo.WinFormsUI.Strings", typeof(Strings).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
