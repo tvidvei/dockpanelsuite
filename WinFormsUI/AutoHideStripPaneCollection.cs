f13f5eff36eb1a58878bb33836490ada6c0a8492
tvidvei <tor.vidvei@gmail.com>
2025-05-18 12:45:49 +0200

    minor cleanup

--- WinFormsUI/AutoHideStripPaneCollection.cs
+++ WinFormsUI/AutoHideStripPaneCollection.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WeifenLuo.Docking
 {
-    [SuppressMessage("Microsoft.Design", "CA1034:NestedTypesShouldNotBeVisible")]
+
     public sealed class AutoHideStripPaneCollection : IEnumerable<AutoHideStripPane>
     {
         private class AutoHideState
