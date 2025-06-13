3dcfb6725e8ce9f3505ba01b01ad22a312714f45
tvidvei <tor.vidvei@gmail.com>
2025-06-13 13:49:48 +0200

    Minor cleaning

---
+++ Utilities/ExpandableObjectWithDynamicDefaultsConverter.cs
@@ -0,0 +1,126 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace Utilities
+{
+
+    public class ExpandableObjectWithDynamicDefaultsConverter : ExpandableObjectConverter
+    {
+        public override PropertyDescriptorCollection GetProperties(ITypeDescriptorContext? context, object value, Attribute[]? attributes)
+        {
+            var pdc = TypeDescriptor.GetProperties(value, attributes);
+            var pdA = new PropertyDescriptor[pdc.Count];
+            for (int i = 0; i < pdc.Count; i++) pdA[i] = new PropertyWithDynamicDefaultsDescriptor(pdc[i]);
+            return new PropertyDescriptorCollection(pdA, readOnly: true);
+        }
+    }
+
+
+    [System.AttributeUsage(System.AttributeTargets.Property)]
+    public class DefaultValuePropertyAttribute : Attribute
+    {
+
+        /// <summary>
+        /// Name of property to be used as the value
+        /// </summary>
+        public string? DefaultValuePropertyName { get; set; }
+
+        /// <summary>
+        /// Name of field or property holding the assigned value
+        /// </summary>
+        /// If AssignedValue is not given, it is assumed to be same as the property name with a lower case first letter 
+        public string? AssignedValueFieldName { get; set; }
+
+        public DefaultValuePropertyAttribute() { }
+
+        public DefaultValuePropertyAttribute(string? defaultValuePropertyName, string? assignedValueFieldName = null)
+        {
+            DefaultValuePropertyName = defaultValuePropertyName;
+            AssignedValueFieldName = assignedValueFieldName;
+        }
+    }
+
+
+    /// <summary>
+    /// PropertyDescriptor with handling of dynamic default value given by a property
+    /// </summary>
+    /// The property must be set up with a backing field holding the assigned value and a Get-method of the following form:
+    /// Get => assignedValue == default(typeof(T)) ? defaultValueProperty : assignedValue;  where T is the type of the property
+    /// The default value is given by a property identified by either adding "Default" to the property name, or a name given
+    /// in the DefaultValuePropertyAttribute. The given value has
+    public class PropertyWithDynamicDefaultsDescriptor : PropertyDescriptor
+    {
+
+        private PropertyDescriptor pd;
+
+        public bool HasDynamicDefault => DefaultValueProperty != null;
+
+        public PropertyInfo? DefaultValueProperty = null;
+        public FieldInfo? AssignedValueField = null;
+        public object? TypeDefaultValue;
+
+        public PropertyWithDynamicDefaultsDescriptor(PropertyDescriptor propertyDescription) : base(propertyDescription)
+        {
+            pd = propertyDescription;
+            string assignedValueName = Name[0].ToString().ToLower() + Name.Substring(1);             // Default name for assigned value field
+            string defaultValueName = assignedValueName + "Default";                                 // Default name for default value property
+
+            var ddAttrs = pd.Attributes.OfType<DefaultValuePropertyAttribute>();
+            if (ddAttrs.Count() > 0)
+            {
+                var ddAttr = ddAttrs.First();
+                if (ddAttr.AssignedValueFieldName != null) assignedValueName = ddAttr.AssignedValueFieldName;
+                if (ddAttr.DefaultValuePropertyName != null) defaultValueName = ddAttr.DefaultValuePropertyName;
+
+            }
+
+            TypeDefaultValue = Activator.CreateInstance(PropertyType);
+            DefaultValueProperty = ComponentType.GetProperty(defaultValueName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+            AssignedValueField = ComponentType.GetField(assignedValueName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        public override Type ComponentType => pd.ComponentType;
+
+        public override bool IsReadOnly => pd.IsReadOnly;
+
+        public override Type PropertyType => pd.PropertyType;
+
+        public override bool CanResetValue(object component)
+        {
+            return pd.CanResetValue(component);
+        }
+
+        public override object? GetValue(object? component)
+        {
+            return pd.GetValue(component);
+        }
+
+        public override void ResetValue(object component)
+        {
+            if (HasDynamicDefault) pd.SetValue(component, TypeDefaultValue);
+            else pd.ResetValue(component);
+        }
+
+        public override void SetValue(object? component, object? value)
+        {
+            pd.SetValue(component, value);
+        }
+
+        public object? GetDefaultValue(object? component)
+        {
+            return HasDynamicDefault ? DefaultValueProperty?.GetValue(component) : TypeDefaultValue;  // Todo: Må håndtere defaultverdi fra DefaultValueAttribute, og uten som er TypeDefaultValue
+        }
+
+
+        public override bool ShouldSerializeValue(object component)
+        {
+            return
+                HasDynamicDefault ? !String.IsNullOrEmpty(Converter.ConvertToString(AssignedValueField!.GetValue(component)))
+                                    : pd.ShouldSerializeValue(component);
+        }
+
+    }
+
+
+
+}
--- WeifenLuo.Docking/Theming/DockPanelColorPalette.cs
+++ WeifenLuo.Docking/Theming/DockPanelColorPalette.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.DirectoryServices;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
-using System.Reflection;
+﻿using System.ComponentModel;
+using Utilities;
 
 namespace WeifenLuo.Docking
 {
@@ -183,179 +175,11 @@ namespace WeifenLuo.Docking
 
     }
 
-    class ColorsTypeDescriptionProvider : TypeDescriptionProvider
-    {
-        private static TypeDescriptionProvider defaultTypeProvider =
-                       TypeDescriptor.GetProvider(typeof(Colors));
 
-        public ColorsTypeDescriptionProvider() : base(defaultTypeProvider)
-        {
-        }
-
-        public override ICustomTypeDescriptor? GetTypeDescriptor(Type objectType, object? instance)
-        {
-            ICustomTypeDescriptor? defaultDescriptor = base.GetTypeDescriptor(objectType, instance);
-
-            return instance == null ? defaultDescriptor :
-                new ColorsCustomTypeDescriptor(defaultDescriptor, instance);
-        }
-
-        class ColorsCustomTypeDescriptor : CustomTypeDescriptor
-        {
-            public ColorsCustomTypeDescriptor(ICustomTypeDescriptor? parent, object instance)
-                : base(parent)
-            {
-            }
-
-            public override PropertyDescriptorCollection GetProperties()
-            {
-                var pdc = base.GetProperties();
-                return pdc;
-            }
-
-            public override PropertyDescriptorCollection GetProperties(Attribute[]? attributes)
-            {
-                var pdc = base.GetProperties(attributes);
-                var pdA = new PropertyDescriptor[pdc.Count];
-                for(int i = 0; i < pdc.Count; i++)
-                {
-                    var pd = pdc[i];
-                    if (pd.Name == "MenuBackground")
-                    {
-                        pd = new PropertyWithDynamicDefaultsDescriptor(pd);
-                    }
-                    pdA[i] = pd;
-                }
-                var pdc2 = new PropertyDescriptorCollection(pdA, readOnly: true);
-                return pdc2;
-            }
-
-        }
-
-    }
-
-    /// <summary>
-    /// PropertyDescription with handling of dynamic default values
-    /// </summary>
-    /// The property must be set up with a backing field holding the assigned value and a Get-method of the following form:
-    /// Get => assignedValue == default(typeof(T)) ? defaultValueProperty : assignedValue;  where T is the type of the property
-    /// The default value is given by a property identified by either adding "Default" to the property name, or a name given
-    /// in the DefaultValuePropertyAttribute. The given value has
-    public class PropertyWithDynamicDefaultsDescriptor : PropertyDescriptor
-    {
-
-        private PropertyDescriptor pd;
-
-        public bool HasDynamicDefault => DefaultValueProperty != null;
-
-        public PropertyInfo? DefaultValueProperty = null;
-        public FieldInfo? AssignedValueField = null;
-        public object? TypeDefaultValue;
-
-        public PropertyWithDynamicDefaultsDescriptor(PropertyDescriptor propertyDescription) : base(propertyDescription)
-        {
-            pd = propertyDescription;
-            string assignedValueName = Name[0].ToString().ToLower() + Name.Substring(1);             // Default name for assigned value field
-            string defaultValueName = assignedValueName + "Default";                                 // Default name for default value property
-
-            var ddAttrs = pd.Attributes.OfType<DefaultValuePropertyAttribute>();
-            if (ddAttrs.Count() > 0)
-            {
-                var ddAttr = ddAttrs.First();
-                if (ddAttr.AssignedValueFieldName != null) assignedValueName = ddAttr.AssignedValueFieldName;
-                if (ddAttr.DefaultValuePropertyName != null) defaultValueName = ddAttr.DefaultValuePropertyName;
-
-            }
-
-            TypeDefaultValue = Activator.CreateInstance(PropertyType);
-            DefaultValueProperty = ComponentType.GetProperty(defaultValueName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
-            AssignedValueField = ComponentType.GetField(assignedValueName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-        }
-
-        public override Type ComponentType => pd.ComponentType;
-
-        public override bool IsReadOnly => pd.IsReadOnly;
-
-        public override Type PropertyType => pd.PropertyType;
-
-        public override bool CanResetValue(object component)
-        {
-            return pd.CanResetValue(component);
-        }
-
-        public override object? GetValue(object? component)
-        {
-            return pd.GetValue(component);
-        }
-
-        public override void ResetValue(object component)
-        {
-            if (HasDynamicDefault) pd.SetValue(component, TypeDefaultValue);
-            else pd.ResetValue(component);
-        }
-
-        public override void SetValue(object? component, object? value)
-        {
-            pd.SetValue(component,value);
-        }
-
-        public object? GetDefaultValue(object? component)
-        {
-            return HasDynamicDefault ? DefaultValueProperty?.GetValue(component) : TypeDefaultValue;  // Todo: Må håndtere defaultverdi fra DefaultValueAttribute, og uten som er TypeDefaultValue
-        }
-
-
-        public override bool ShouldSerializeValue(object component)
-        {
-            return 
-                HasDynamicDefault   ? !String.IsNullOrEmpty(Converter.ConvertToString(AssignedValueField!.GetValue(component))) 
-                                    : pd.ShouldSerializeValue(component);
-        }
-
-    }
-
-
-    [System.AttributeUsage(System.AttributeTargets.Property)]
-    public class DefaultValuePropertyAttribute : Attribute
-    {
-
-        /// <summary>
-        /// Name of property to be used as the value
-        /// </summary>
-        public string? DefaultValuePropertyName { get; set; }
-
-        /// <summary>
-        /// Name of field or property holding the assigned value
-        /// </summary>
-        /// If AssignedValue is not given, it is assumed to be same as the property name with a lower case first letter 
-        public string? AssignedValueFieldName {  get; set; }
-
-        public DefaultValuePropertyAttribute() { }
-
-        public DefaultValuePropertyAttribute(string? defaultValueName, string? assignedValueName = null) { 
-            DefaultValuePropertyName = defaultValueName;
-            AssignedValueFieldName = assignedValueName;
-        }
-    }
-
-
-    public class ExpandableObjectWithDynamicDefaultsConverter : ExpandableObjectConverter
-    {
-        public override PropertyDescriptorCollection GetProperties(ITypeDescriptorContext? context, object value, Attribute[]? attributes)
-        {
-            var pdc = TypeDescriptor.GetProperties(value, attributes);
-            var pdA = new PropertyDescriptor[pdc.Count];
-            for (int i = 0; i < pdc.Count; i++) pdA[i] = new PropertyWithDynamicDefaultsDescriptor(pdc[i]);
-            return new PropertyDescriptorCollection(pdA, readOnly: true);
-        }
-    }
-
-
-
-    //[TypeDescriptionProvider(typeof(ColorsTypeDescriptionProvider))]
     /// <summary>
     ///  Color palette that is source for all other colors
     /// </summary>
+    [TypeConverter(typeof(ExpandableObjectWithDynamicDefaultsConverter))]
     public class Colors
     {
 
@@ -369,7 +193,7 @@ namespace WeifenLuo.Docking
 
 
         [Description("Background color for menues and toolbars")]
-        [DefaultValueProperty("ToolsBackground")]
+        [DefaultValueProperty(nameof(ToolsBackground))]
         public Color MenuBackground {
             get { return menuBackground != default(Color) ? menuBackground : ToolsBackground; }
             set { menuBackground = value; }
@@ -394,8 +218,6 @@ namespace WeifenLuo.Docking
 
         [Description("Borders around docked windows.\r\nHide borders by setting this equal to MainBackground")]
         public Color MainBorders { get; set; } = Color.FromArgb(93, 107, 153);
-
-
 
         [Description("Default background color for Captions and and Tabs.  Used when inactive and not selected")]
         public Color TabBackgroundNormal { get; set; } = Color.FromArgb(64, 86, 141);
